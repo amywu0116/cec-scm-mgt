a6342def80d5c60945e44863df7f932184c81c87
EdwardHsu221001 <edward_hsu@syscom.com.tw>
2024-05-13 17:11:39 +0800

    串接 API

--- app/page.js
+++ app/page.js
@@ -1,71 +1,19 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { redirect, useRouter } from "next/navigation";
-import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  ShopOutlined,
-  TeamOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-import { Button, Layout, Menu, theme } from "antd";
+import { Button, Layout, theme } from "antd";
 
+import Sider from "./Sider";
 import api from "@/api";
 
-const { Header, Content, Footer, Sider } = Layout;
-
-const menuList = [
-  { key: "sub1", label: "公告欄", icon: React.createElement(UserOutlined) },
-  { key: "sub2", label: "儀表板", icon: React.createElement(UserOutlined) },
-  {
-    key: "sub3",
-    label: "促銷",
-    icon: React.createElement(UserOutlined),
-    children: [],
-  },
-  {
-    key: "sub4",
-    label: "訂單",
-    icon: React.createElement(UserOutlined),
-    children: [],
-  },
-  {
-    key: "sub5",
-    label: "商品",
-    icon: React.createElement(UserOutlined),
-    children: [],
-  },
-  {
-    key: "sub6",
-    label: "財務",
-    icon: React.createElement(UserOutlined),
-    children: [],
-  },
-];
-
-const items = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  AppstoreOutlined,
-  TeamOutlined,
-  ShopOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: React.createElement(icon),
-  label: `nav ${index + 1}`,
-}));
+const { Header, Content, Footer } = Layout;
 
 const Page = () => {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
   const router = useRouter();
+  const accessToken = localStorage.getItem("cec-scm-mgt-accessToken");
 
   const handleLogout = () => {
     const accessToken = localStorage.getItem("cec-scm-mgt-accessToken");
@@ -86,30 +34,17 @@ const Page = () => {
       .finally(() => {});
   };
 
-  // if (true) {
-  //   redirect("/login");
-  // }
+  // useEffect(() => {
+  //   if (!accessToken) {
+  //     redirect("/login");
+  //   }
+  // }, [accessToken]);
+
+  console.log("accessToken", accessToken);
 
   return (
     <Layout hasSider>
-      <Sider
-        style={{
-          overflow: "auto",
-          height: "100vh",
-          position: "fixed",
-          left: 0,
-          top: 0,
-          bottom: 0,
-        }}
-      >
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["4"]}
-          items={menuList}
-        />
-      </Sider>
+      <Sider />
 
       <Layout
         style={{
