523eb397f5825e0de78c60071518f77f286cb1aa
EdwardHsu221001 <edward_hsu@syscom.com.tw>
2024-08-19 14:19:13 +0800

    解決偶爾跑版問題

--- app/(system)/layout.js
+++ app/(system)/layout.js
@@ -2,8 +2,8 @@
 import { ConfigProvider, Layout } from "antd";
 import zhTW from "antd/locale/zh_TW";
 import "dayjs/locale/zh-tw";
-import { Suspense, useEffect, useState } from "react";
-import styled, { css } from "styled-components";
+import { Suspense, useEffect } from "react";
+import styled from "styled-components";
 
 import Sider from "./Sider";
 
@@ -11,17 +11,12 @@ import api from "@/api";
 import { useBoundStore } from "@/store";
 
 const Container = styled.div`
-  ${(props) =>
-    props.$headerHeight &&
-    css`
-      margin: 124px 36px 33px;
-    `}
+  margin: 124px 36px 33px;
 `;
 
 export default function PageLayout(props) {
   const { children } = props;
 
-  const [headerHeight, setHeaderHeight] = useState(100);
   const updateOptions = useBoundStore((state) => state.updateOptions);
 
   const fetchOptions = () => {
@@ -31,12 +26,6 @@ export default function PageLayout(props) {
       .catch((err) => console.log(err))
       .finally(() => {});
   };
-
-  useEffect(() => {
-    const header = document.querySelector("header.ant-layout-header");
-    const headerHeight = header?.getBoundingClientRect().height;
-    setHeaderHeight(headerHeight);
-  }, []);
 
   useEffect(() => {
     if (updateOptions) {
@@ -57,7 +46,7 @@ export default function PageLayout(props) {
           }}
         >
           <ConfigProvider locale={zhTW}>
-            <Container $headerHeight={headerHeight}>{children}</Container>
+            <Container>{children}</Container>
           </ConfigProvider>
         </Layout>
       </Layout>
